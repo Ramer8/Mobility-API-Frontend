de5ebf90e721fa0b0446714538d16b3ad602f560
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-05-06 04:53:36 +0200

    Feat:created footer Data bar

--- src/common/Maps/Maps.jsx
+++ src/common/Maps/Maps.jsx
@@ -139,7 +139,7 @@ const Maps = () => {
                 <input
                   className="inputDesign destination"
                   type="text"
-                  placeholder="Destination"
+                  placeholder="Enter your destination"
                   ref={destiantionRef}
                 ></input>
               </Autocomplete>
@@ -191,7 +191,7 @@ const Maps = () => {
               />
             </div>
 
-            <div className="buttonBar2">
+            <div className="buttonBarBottom">
               <CustomButton
                 className={"primaryButton uploadProfile calculateRouteButton"}
                 title={"Calculate Route"}
@@ -219,10 +219,6 @@ const Maps = () => {
                 }}
               />
             </div>
-            <div className="infoBar">
-              <div>Distance: {distance} </div>
-              <div>Duration: {duration} </div>
-            </div>
           </div>
 
           <GoogleMap
@@ -243,6 +239,112 @@ const Maps = () => {
               <DirectionsRenderer directions={directionsResponse} />
             )}
           </GoogleMap>
+          {distance && (
+            <div className="footerData">
+              <div className="leftColumn">
+                <div className="carIcon">
+                  <img src="/carM.png" width={80} alt="carM" />
+                </div>
+                <div className="Data">
+                  <div className="titleData">Car</div>
+                  <div className="rowTitleData">
+                    <div>In 10 min</div>
+                    <div>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-person-plus"
+                        viewBox="0 -2 16 16"
+                      >
+                        <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+                        <path
+                          fillRule="evenodd"
+                          d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5"
+                        />
+                      </svg>{" "}
+                      4
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="infoBar">
+                <div className="rowInfoBar">
+                  <div className="titleInfoBar">
+                    <svg
+                      width="22px"
+                      height="22px"
+                      viewBox="0 0 64 64"
+                      xmlns="http://www.w3.org/2000/svg"
+                      strokeWidth="3"
+                      stroke="darkgray"
+                      fill="currentColor"
+                    >
+                      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                      <g
+                        id="SVGRepo_tracerCarrier"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      ></g>
+                      <g id="SVGRepo_iconCarrier">
+                        <path d="M17.94,54.81a.1.1,0,0,1-.14,0c-1-1.11-11.69-13.23-11.69-21.26,0-9.94,6.5-12.24,11.76-12.24,4.84,0,11.06,2.6,11.06,12.24C28.93,41.84,18.87,53.72,17.94,54.81Z"></path>
+                        <circle cx="17.52" cy="31.38" r="4.75"></circle>
+                        <path d="M49.58,34.77a.11.11,0,0,1-.15,0c-.87-1-9.19-10.45-9.19-16.74,0-7.84,5.12-9.65,9.27-9.65,3.81,0,8.71,2,8.71,9.65C58.22,24.52,50.4,33.81,49.58,34.77Z"></path>
+                        <circle cx="49.23" cy="17.32" r="3.75"></circle>
+                        <path d="M17.87,54.89a28.73,28.73,0,0,0,3.9.89"></path>
+                        <path
+                          d="M24.68,56.07c2.79.12,5.85-.28,7.9-2.08,5.8-5.09,2.89-11.25,6.75-14.71a16.72,16.72,0,0,1,4.93-3"
+                          strokeDasharray="7.8 2.92"
+                        ></path>
+                        <path d="M45.63,35.8a23,23,0,0,1,3.88-.95"></path>
+                      </g>
+                    </svg>
+                  </div>
+                  <div> {distance}</div>
+                </div>
+                <div className="rowInfoBar">
+                  <div className="titleInfoBar">
+                    <svg
+                      width="16"
+                      height="16"
+                      viewBox="0 0 48 48"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                    >
+                      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                      <g
+                        id="SVGRepo_tracerCarrier"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      ></g>
+                      <g id="SVGRepo_iconCarrier">
+                        {" "}
+                        <title>time-update</title>{" "}
+                        <g id="Layer_2" data-name="Layer 2">
+                          {" "}
+                          <g id="invisible_box" data-name="invisible box">
+                            {" "}
+                            <rect
+                              width="48"
+                              height="48"
+                              fill="none"
+                            ></rect>{" "}
+                          </g>{" "}
+                          <g id="icons_Q2" data-name="icons Q2">
+                            {" "}
+                            <path d="M2,24a22,22,0,0,0,41.7,9.7,2,2,0,0,0-.5-2.6,2,2,0,0,0-3,.7A17.8,17.8,0,0,1,24,42,18,18,0,1,1,37.4,12H32.1A2.1,2.1,0,0,0,30,13.7,2,2,0,0,0,32,16H42a2,2,0,0,0,2-2V4.1A2.1,2.1,0,0,0,42.3,2,2,2,0,0,0,40,4V8.9A22,22,0,0,0,2,24Z"></path>{" "}
+                            <path d="M34,32a1.7,1.7,0,0,1-1-.3L22,25.1V14a2,2,0,0,1,4,0v8.9l9,5.4a1.9,1.9,0,0,1,.7,2.7A1.9,1.9,0,0,1,34,32Z"></path>{" "}
+                          </g>{" "}
+                        </g>{" "}
+                      </g>
+                    </svg>{" "}
+                  </div>
+                  <div>{duration}</div>
+                </div>
+              </div>
+            </div>
+          )}
         </>
       )}
     </>
