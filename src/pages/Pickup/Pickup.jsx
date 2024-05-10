e171e8bc0731161647fbc7bd20d9f1cb60d743fa
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-05-10 10:24:25 +0200

    Update, trip with hardcode id show data fetched from data base

--- src/pages/Pickup/Pickup.jsx
+++ src/pages/Pickup/Pickup.jsx
@@ -126,26 +126,28 @@ const Pickup = () => {
           <div className="pickupBody">
             <div className="bodyLeft">
               <div className="bodyLeftTop">
-                <b>
+                <div className="driverName">
                   {DriverNameSplited.name} {DriverNameSplited.last}
-                </b>{" "}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="gold"
-                  className="bi bi-star-fill"
-                  viewBox="0 0 16 16"
-                >
-                  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                </svg>
-                {"4.9"}
+                </div>
+                <div className="driverScore">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="gold"
+                    className="bi bi-star-fill starIcon"
+                    viewBox="0 1 16 16"
+                  >
+                    <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                  </svg>{" "}
+                  {!trip.driverScore ? "0" : trip.driverScore}
+                </div>
               </div>
               <div className="carBrandModel">
-                {"Car-Brand-"}
-                {"model Car"}
+                {/* {"Car-Brand-"} */}
+                {trip.carModel}
               </div>
-              <div className="numberPlate">{"5632 mbw"}</div>
+              <div className="numberPlate">{trip.carNumberPlate}</div>
             </div>
             <div className="bodyright">
               <img
@@ -180,6 +182,7 @@ const Pickup = () => {
                 className="chatIcon"
                 src="./public/icons/chatIcon.svg"
                 width={45}
+                color="#8492a6"
                 alt="chatIcon"
               ></img>
               <CustomInput
