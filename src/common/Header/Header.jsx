a8689874d25b6b4329c690dde35c99f02c019a6b
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-05-11 18:46:53 +0200

    Update: dropdown added icon to log in menu

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -221,7 +221,6 @@ export const Header = () => {
                   />
                 )}
               </div>
-              {/* <a href="#about">About</a> */}
               <CustomLink
                 title={
                   <div className="dropdownIcons">
@@ -240,7 +239,6 @@ export const Header = () => {
                 }
                 destination="/"
               />
-              {/* <a href="#contact">Contact</a> */}
               {rdxUser?.credentials.token ? (
                 <div className="menu">
                   <CustomLink
@@ -308,7 +306,28 @@ export const Header = () => {
               ) : (
                 <div className="menu">
                   <CustomLink
-                    title={"Login"}
+                    title={
+                      <div className="dropdownIcons">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-box-arrow-left"
+                          viewBox="0 -2 16 16"
+                        >
+                          <path
+                            fillRule="evenodd"
+                            d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"
+                          />
+                          <path
+                            fillRule="evenodd"
+                            d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"
+                          />
+                        </svg>{" "}
+                        Login
+                      </div>
+                    }
                     destination="/login"
                     className={`${
                       location.pathname === "/login"
