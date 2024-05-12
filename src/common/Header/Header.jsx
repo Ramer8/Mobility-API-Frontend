b9b3d2ad38a55bc4c83d1b603b1e56534373fe12
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-05-12 17:39:54 +0200

    Update: layout changes

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -48,8 +48,8 @@ export const Header = () => {
         <div className="arrow-icon" onClick={() => navigate(-1)}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="20"
-            height="20"
+            width="16"
+            height="16"
             fill="currentColor"
             className="bi bi-arrow-left"
             viewBox="0 0 16 16"
