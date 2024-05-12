50c4be9177fa28d06906495346e32e979a061751
Ramer8 <75700901+Ramer8@users.noreply.github.com>
2024-05-12 16:52:31 +0200

    Upddate: calculate route button layout changed

--- src/common/Maps/Maps.jsx
+++ src/common/Maps/Maps.jsx
@@ -335,34 +335,36 @@ const Maps = () => {
             </div>
 
             <div className="buttonBarBottom">
-              <CustomButton
-                // className={"primaryButton uploadProfile calculateRouteButton"}
-                className={" calculateRouteButton"}
-                title={"Calculate Route"}
-                functionEmit={() => {
-                  calculateRoute()
-                }}
-              />
-              <CustomButton
-                // className={"primaryButton uploadProfile clearRouteButton"}
-                className={"clearRouteButton"}
-                title={
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    className="bi bi-x-circle"
-                    viewBox="0 0 16 16"
-                  >
-                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-                  </svg>
-                }
-                functionEmit={() => {
-                  clearRoute()
-                }}
-              />
+              <div className="leftSide"></div>
+              <div className="rightSide">
+                <CustomButton
+                  // className={"primaryButton uploadProfile calculateRouteButton"}
+                  className={" calculateRouteButton"}
+                  title={"Calculate Route"}
+                  functionEmit={() => {
+                    calculateRoute()
+                  }}
+                />
+                <CustomButton
+                  // className={"primaryButton uploadProfile clearRouteButton"}
+                  className={"clearRouteButton"}
+                  title={
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="35"
+                      height="35"
+                      fill="currentColor"
+                      className="bi bi-x"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
+                    </svg>
+                  }
+                  functionEmit={() => {
+                    clearRoute()
+                  }}
+                />
+              </div>
             </div>
           </div>
           {location && (
